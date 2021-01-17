fb81dd2305726ce96f0d755a5f1c4d990dd68e7a
yusuf <ysfozturk3@gmail.com>
2021-01-17 23:42:12 +0100

    register completed

--- src/users/serializer.py
+++ src/users/serializer.py
@@ -6,7 +6,7 @@ from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required = True, validators = [
-                                    UniqueValidator(queryset=User.object.all())])
+                                    UniqueValidator(queryset=User.objects.all())])
     
     password = serializers.CharField(write_only =True,required = True,validators=[
                                 validate_password],style = {'input_type':'password'})
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializer import RegisterSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset =User.objects.all()
+    serializer_class = RegisterSerializer
