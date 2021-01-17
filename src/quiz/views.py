9cb38ac836ee854f47e0485b2330128261c55c40
yusuf <ysfozturk3@gmail.com>
2021-01-17 15:55:43 +0100

    added quiz detail and pagination

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,8 +1,9 @@
 from django.db.models import query
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Category, Quiz
-from .serializer import CategorySerializer, CategoryDetailSerializer
+from .models import Category, Question, Quiz
+from .serializer import CategorySerializer, CategoryDetailSerializer, QuestionSerializer
+from .pagination import MyPagination
 
 # Create your views here.
 
@@ -18,4 +19,15 @@ class CategoryDetail(generics.ListAPIView):
         category =self.kwargs['category']
         queryset = queryset.filter(category__name = category)
         return queryset
+    
 
+class QuizDetail(generics.ListAPIView):
+    serializer_class = QuestionSerializer
+    pagination_class = MyPagination
+    
+    def get_queryset(self):
+        querySet = Question.objects.all()
+        title = self.kwargs['title']
+        querySet = querySet.filter(quiz__title = title)
+        return querySet
+        
