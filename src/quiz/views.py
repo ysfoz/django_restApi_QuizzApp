fdc3eb7aa847d7564261851723a0f87928471f5d
yusuf <ysfozturk3@gmail.com>
2021-01-18 21:47:36 +0100

    solved a bug

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Category, Question, Quiz
 from .serializer import CategorySerializer, CategoryDetailSerializer, QuestionSerializer
-from .pagination import MyPagination
+# from .pagination import MyPagination
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class CategoryList(generics.ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsAuthenticated]
     
 class CategoryDetail(generics.ListAPIView):   
     serializer_class = CategoryDetailSerializer
+    permission_classes = [IsAuthenticated]
   
     def get_queryset(self):
         queryset = Quiz.objects.all()
@@ -23,7 +26,8 @@ class CategoryDetail(generics.ListAPIView):
 
 class QuizDetail(generics.ListAPIView):
     serializer_class = QuestionSerializer
-    pagination_class = MyPagination
+    # pagination_class = MyPagination
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         querySet = Question.objects.all()
